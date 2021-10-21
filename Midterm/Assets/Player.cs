221a97527c870ae9b646ae9f0cd2df5bb4596b10
novakc <novakc@iu.edu>
2021-10-20 20:51:57 -0400

    milestone 1

---
+++ Midterm/Assets/Player.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] float _mouseSensitivity = 10f;
+    [SerializeField] float _moveSpeed = 3f;
+    [SerializeField] Camera _cam;
+    float _currentTilt = 0f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Aim();
+        Movement();
+    }
+
+    void Aim()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        transform.Rotate(Vector3.up, mouseX * _mouseSensitivity);
+
+        _currentTilt -= mouseY * _mouseSensitivity;
+        _currentTilt = Mathf.Clamp(_currentTilt, -90, 90);
+        _cam.transform.localEulerAngles = new Vector3(_currentTilt, 0, 0);
+    }
+
+    void Movement()
+    {
+        Vector3 sidewaysMovementVector = transform.right * Input.GetAxis("Horizontal");
+        Vector3 forwardBackwardMovementVector = transform.forward * Input.GetAxis("Vertical");
+        Vector3 movementVector = sidewaysMovementVector + forwardBackwardMovementVector;
+
+        GetComponent<CharacterController>().Move(movementVector * _moveSpeed * Time.deltaTime);
+    }
+}
